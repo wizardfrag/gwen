c6afd890a178f994e6b83300373f90b2c28792b2
David White <david@rartm.eu>
2014-06-15 18:21:39 +0100

    Changed io:format and lager:infos to lager:debug

--- src/gwen_db.erl
+++ src/gwen_db.erl
@@ -17,15 +17,15 @@ start(Args) ->
 	gen_server:start_link({local, ?MODULE}, ?MODULE, [Args], []).
 
 stop() ->
-	lager:info("Stopping Riak DB handler..."),
+	lager:debug("Stopping Riak DB handler..."),
 	gen_server:cast(?MODULE, shutdown).
 
 
 init(_) ->
 	process_flag(trap_exit, true),
-	lager:info("Connecting to Riak..."),
+	lager:debug("Connecting to Riak..."),
 	{ok, Pid} = riakc_pb_socket:start_link("127.0.0.1", 8087),
-	lager:info("Connected to Riak!"),
+	lager:debug("Connected to Riak!"),
 	State = #state{conn=Pid},
 	pong = riakc_pb_socket:ping(Pid),
 	{ok, State}.
@@ -35,21 +35,21 @@ handle_call({put, Object}, _From, #state{conn=Conn}=State) ->
 	Result = riakc_pb_socket:put(Conn, Object),
 	{reply, Result, State};
 handle_call(Message, From, State) ->
-	lager:info("Call: ~p from ~p with state ~p", [Message, From, State]),
+	lager:debug("Call: ~p from ~p with state ~p", [Message, From, State]),
 	{reply, ok, State}.
 
 handle_cast(shutdown, State) ->
 	{stop, normal, State};
 handle_cast(Message, State) ->
-	lager:info("Cast: ~p with state ~p", [Message, State]),
+	lager:debug("Cast: ~p with state ~p", [Message, State]),
 	{noreply, State}.
 
 handle_info(Message, State) ->
-	io:format("Info: ~p with state ~p", [Message, State]),
+	lager:debug("Info: ~p with state ~p", [Message, State]),
 	{noreply, State}.
 
 terminate(_Reason, _State) ->
-	io:format("Terminating...").
+	lager:fatal("Terminating...").
 
 code_change(_OldVsn, State, _Extra) ->
 	{ok, State}.
